79d732d32d51dc2d88774d4bb80bafc2a9329ee9
Abimanyu-A <itsactuallymeabi@gmail.com>
2024-11-18 11:17:38 +0530

    controller and routes

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+//jane waqt mujhse milke janaa - middleware
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+export const upload = multer({ storage: storage })
